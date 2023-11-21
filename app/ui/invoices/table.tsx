c2880cf1fef74613eefdb6f8c2d412c1a10fc270
vikasdb1999 <vikasdubey1999@gmail.com>
2023-11-22 03:24:23 +0530

    j

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -26,13 +26,14 @@ export default async function InvoicesTable({
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
-                      <Image
-                        src={invoice.image_url}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={` profile picture`}
-                      />
+                    <Image
+  src={invoice.image_url}
+  className="rounded-full"
+  width={28}
+  height={28}
+  alt={invoice.name} // Provide a suitable alt text here
+/>
+
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>

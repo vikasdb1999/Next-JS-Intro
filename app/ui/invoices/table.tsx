37da792e2f46b66d8a9b5dc657137f1ceba476d0
vikasdb1999 <vikasdubey1999@gmail.com>
2023-11-22 03:33:48 +0530

    ff

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -91,6 +91,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt="Test"
                       />
                       <p>{invoice.name}</p>
                     </div>
